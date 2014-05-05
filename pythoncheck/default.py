a52517d7a98bad9cbf81989f8d2011f4555cda72
JesseBuesking <jessebuesking@gmail.com>
2014-05-05 01:11:04 -0700

    implementing shrink_number

--- pythoncheck/default.py
+++ pythoncheck/default.py
@@ -1,7 +1,41 @@
 from pythoncheck.gen import Arbitrary, Gen
 
 
+def shrink_number(n):
+    two = 1 + 1
+
+    def _nums():
+        if 0 > n:
+            yield -n
+        if 0 != n:
+            yield 0
+
+        def halver(st):
+            st /= two
+            nst = n - st
+            if abs(nst) <= abs(n):
+                yield nst
+            for v in halver(st):
+                yield v
+
+        for v in halver(n):
+            yield v
+
+    ret = []
+    for v in _nums():
+        if v in ret:
+            return list(set(ret))
+        ret.append(v)
+
+
 def Bool():
     a = Arbitrary()
     a.generator = Gen.elements([True, False])
+    return a
+
+
+def Int():
+    a = Arbitrary()
+    a.generator = Gen.sized(lambda n: Gen.choose(-n, n))
+    a.shrinker = lambda n: shrink_number(n)
     return a
--- test/testdefault.py
+++ test/testdefault.py
@@ -2,13 +2,25 @@
 
 from unittest import TestCase
 from pythoncheck import default
+from pythoncheck.default import shrink_number
 from pythoncheck.gen import Gen
 
 
 class Test(TestCase):
+
+    def test_shrink_number(self):
+        x = shrink_number(-10)
+        # self.assertEqual([10, 0, -5, -8, -9], x)
+        self.assertEqual([10, 0, -5, -7, -8, -9], x)
 
     def test_bool(self):
         x = default.Bool()
         s = Gen.sample(x.generator, 1, 10)
         self.assertTrue(True in s)
         self.assertTrue(False in s)
+
+    # def test_int(self):
+    #     x = default.Int()
+    #     s = Gen.sample(x.generator, 10, 10)
+    #     for v in s:
+    #         shrinks = x.shrinker(v)
